79a8bdb70638a7bd830a2d970ffcd1a9e1a46a7b
ravindra <sirviravindra609@gmail.com>
2023-08-30 22:02:21 +0530

    auth lib deleted

--- ravindra-ngrx/src/app/app.component.ts
+++ ravindra-ngrx/src/app/app.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
-import { AuthModule } from 'auth/src/lib/auth.module';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, MainNavComponent, AuthModule],
+  imports: [NxWelcomeComponent, RouterModule, MainNavComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
